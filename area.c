0ca8ebf1cd7bf54a48e1d19de45da6cf09c0179f
resapu-gnanasree <69516227+resapu-gnanasree@users.noreply.github.com>
2020-08-20 12:07:24 +0530

    Created area.c
    This program is used to find the area of the rectangle and square

---
+++ area.c
@@ -0,0 +1,17 @@
+import java.io.*;
+class area
+{
+    public static void main(String args[])
+    {
+        int side,length,breadth;
+        side=5;
+        int sq_area=side*side;
+        length=10;
+        breadth=6;
+        int rect_area=length*breadth;
+        System.out.println("the area of the square=");
+        System.out.println(sq_area);
+        System.out.println("the area of the rectangle=");
+        System.out.println(rect_area);
+    }
+}
